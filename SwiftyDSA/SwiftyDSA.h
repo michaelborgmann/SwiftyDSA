9fba79749461dbe659c554f606a9e833f8a61e5f
Michael Borgmann <michaelborgmann@mailbox.org>
2020-01-28 10:57:54 +0100

    Initial Commit

---
+++ SwiftyDSA/SwiftyDSA.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDSA.h
+//  SwiftyDSA
+//
+//  Created by Michael Borgmann on 28.01.20.
+//  Copyright © 2020 Michael Borgmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyDSA.
+FOUNDATION_EXPORT double SwiftyDSAVersionNumber;
+
+//! Project version string for SwiftyDSA.
+FOUNDATION_EXPORT const unsigned char SwiftyDSAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDSA/PublicHeader.h>
+
+
